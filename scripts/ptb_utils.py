c087538ace92f4b837eaf8dffda2c8ac01aafc16
Harshay Shah <harshay@MSRI-DNN2.h002u0dvqbouphlczboenl0fxb.xx.internal.cloudapp.net>
2021-02-18 21:37:36 +0000

    uap code and nb

--- scripts/ptb_utils.py
+++ scripts/ptb_utils.py
@@ -234,3 +234,221 @@ class Linf_PGD_Attack(PGD_Attack):
         self.delta.grad.zero_()
         return self.delta
 
+    
+# UAP methods
+
+class AS_UAP(object):
+    """
+    UAP method (Algorithm 2) in Universal Adversarial Training paper (https://arxiv.org/abs/1811.11304)
+    not using clipped version to avoid hyper-parameter tuning
+    (even with tuning, improvement is marginal)
+    """
+
+    def __init__(self, eps, lr, num_iter, shape, num_classes=2, bounds=(0.,1.),
+                 loss_type='untargeted', rand_eps=0., device=None):
+        self.device = device if device else gu.get_device(None)
+        self.loss_type = loss_type
+        self.B = bounds
+        self.rand_eps = rand_eps
+        self.eps = eps
+        self.lr = lr
+        self.num_iter = num_iter
+        self.num_classes = num_classes
+        self.classes = list(range(self.num_classes))
+        self.shape = shape
+        self._init_delta()
+        
+    @property
+    def uap(self):
+        return copy.deepcopy(self.delta.detach().cpu()).numpy()
+
+    def fit(self, dl, model, num_epochs):
+        # compute uap
+        model = model.to(self.device)
+        for t in range(num_epochs):
+            for xb, yb in dl:
+                xb, yb = xb.to(self.device), yb.to(self.device)
+
+                # update + project + clamp delta t times
+                for t in range(self.num_iter):
+                    loss = self._get_loss(xb, yb, model, True)
+                    loss.backward()
+                    self.delta = self._update_delta(xb, yb)
+
+    def evaluate_attack(self, dl, model, **kw):
+        model = model.to(self.device)
+        X, Xa, Ya, P = [], [], [], []
+
+        for xb, yb in dl:
+            xb, yb = xb.to(self.device), yb.to(self.device)
+            xba, ptb = self._perturb(xb, yb, False)
+            X.append(xb.cpu())
+            Xa.append(xba.cpu())
+            Ya.append(yb.cpu())
+            P.append(ptb.cpu())
+
+        X, Xa, Ya, P = map(torch.cat, [X, Xa, Ya, P])
+        ta_dl = utils._to_dl(Xa, Ya, dl.batch_size)
+        acc_func = utils.compute_loss_and_accuracy_from_dl
+        acc, loss = acc_func(ta_dl, model, F.cross_entropy, device=self.device)
+
+        return {
+            'P': P,
+            'X': X,
+            'Xa': Xa,
+            'Ya': Ya,
+            'acc': acc.item(),
+            'loss': loss.item(),
+            'dl': ta_dl
+        }
+
+    def _get_loss(self, xb, yb, model, train_mode):
+        xba, delta = self._perturb(xb, yb, train_mode=train_mode)
+        out = model(xba)
+
+        if self.loss_type == 'untargeted':
+            return -1*nn.CrossEntropyLoss()(out, yb)
+
+        elif self.loss_type == 'targeted':
+            return nn.CrossEntropyLoss()(out, yb)
+
+        elif self.loss_type == 'binary_targeted':
+            yb_opp = 1-yb
+            return nn.CrossEntropyLoss()(out, yb_opp)
+
+        elif self.loss_type ==  'binary_hybrid':
+            yb_opp = 1-yb
+            return nn.CrossEntropyLoss()(out, yb_opp) - nn.CrossEntropyLoss()(out, yb)
+
+        else:
+            assert False, "unknown loss type"
+
+    def _perturb(self, xb, yb, train_mode):
+        # broadcast clamped + scaled + signed-if-binary UAPs
+        d = self.delta if train_mode else self.delta.data
+        sign = ((2*yb-1)*1.0) if self.num_classes == 2 else torch.ones(len(yb)).to(self.device)
+        sign = sign[(...,)+(None,)*(len(xb.shape)-1)].float()
+        delta = torch.zeros_like(xb, device=self.device)
+        delta = sign*(delta+d)
+
+        # perturb and re-clamp data
+        xba = (xb + delta).clamp(self.B[0], self.B[1])
+        delta = xba-xb
+        return xba, delta
+
+    def _init_delta(self):
+        delta = torch.zeros(*self.shape)
+        delta = delta.uniform_(-self.rand_eps, self.rand_eps).to(self.device)
+        delta.requires_grad = True
+        self.delta = delta
+
+class Linf_AS_UAP(AS_UAP):
+
+    def _update_delta(self, xb, yb):
+        # steepest descent + linf projection (GD)
+        self.delta.data = self.delta.data - self.lr*(self.delta.grad.detach().sign())
+        self.delta.data = self.delta.data.clamp(-self.eps, self.eps)
+        self.delta.grad.zero_()
+        return self.delta
+
+class L2_AS_UAP(AS_UAP):
+
+    OVERFLOW_CONST = 1e-10
+
+    def _update_delta(self, xb, yb):
+        # normalize gradients
+        grad = self.delta.grad.detach()
+        norms = grad.norm()
+        grad = grad/(norms+self.OVERFLOW_CONST) # add const to avoid overflow
+
+        # steepest descent
+        self.delta.data = self.delta.data - self.lr*grad
+
+        # l2 ball projection
+        delta_norms = self.delta.data.norm()
+        self.delta.data = self.eps*self.delta.data / (delta_norms.clamp(min=self.eps))
+
+        self.delta.grad.zero_()
+        return self.delta
+
+
+class SVD_UAP(object):
+    # based on https://arxiv.org/abs/2005.08632
+
+    def __init__(self, attack, bounds=(0.,1.), device=None, num_classes=2):
+        self.device = device if device else gu.get_device(None)
+        self.attack = attack
+        self.attack.device = self.device
+        self.B = bounds
+        self.num_classes = num_classes
+
+    def fit(self, dl, model):
+        # get perturbations
+        model = model.to(self.device)
+        pdata = self.attack.evaluate_attack(dl, model)
+
+        self.p_acc = pdata['acc']
+        shape = list(pdata['Xa'].shape)
+        self.num_imgs, self.img_shape = shape[0], shape[1:]
+
+        # run SVD
+        P = pdata['P'].reshape(pdata['P'].shape[0], -1)
+        self.P = P / np.linalg.norm(P, axis=1)[:, None]
+        U, self.S, self.VH = np.linalg.svd(P)
+        del U
+
+        # setup UAP
+        self.uaps = self.VH.reshape(self.VH.shape[0], *self.img_shape)
+        self.uap = self.uaps[0]
+
+    def evaluate_attack(self, dl, model, eps, kth=0, **kw):
+        self.delta = torch.FloatTensor(self.uaps[kth]).to(self.device)
+        eval1 = self._eval(dl, model, eps, 1.0)
+        eval2 = self._eval(dl, model, eps, -1.0)
+        if eval1['acc'] < eval2['acc']: return eval1
+        return eval2
+
+    def _eval(self, dl, model, eps, pos_dir):
+        model = model.to(self.device)
+        X, Xa, Ya, P = [], [], [], []
+
+        for xb, yb in dl:
+            xb, yb = xb.to(self.device), yb.to(self.device)
+            xba, ptb = self._perturb(xb, yb, eps, pos_dir)
+            X.append(xb.cpu())
+            Xa.append(xba.cpu())
+            Ya.append(yb.cpu())
+            P.append(ptb.cpu())
+
+        X, Xa, Ya, P = map(torch.cat, [X, Xa, Ya, P])
+        ta_dl = utils._to_dl(Xa, Ya, dl.batch_size)
+        acc_func = utils.compute_loss_and_accuracy_from_dl
+        acc, loss = acc_func(ta_dl, model, F.cross_entropy, device=self.device)
+
+        return {
+            'P': P,
+            'X': X,
+            'Xa': Xa,
+            'Ya': Ya,
+            'acc': acc.item(),
+            'loss': loss.item(),
+            'dl': ta_dl,
+            'pos_dir': pos_dir
+        }
+
+    def _perturb(self, xb, yb, eps, pos_dir):
+        nch = len(xb.shape)
+
+        # broadcast clamped + scaled + signed UAPs
+        sign = ((2*yb-1)*(1.0)) if self.num_classes == 2 else torch.ones(len(yb)).to(self.device)
+        sign = sign[(...,)+(None,)*(len(xb.shape)-1)].float()
+        
+        delta = torch.zeros_like(xb, device=self.device)
+        delta = eps*pos_dir*sign*(delta + self.delta)
+
+        # perturb and re-clamp data
+        xba = xb + delta
+        xba = xba.clamp(self.B[0], self.B[1])
+        delta = xba-xb
+        return xba, delta
+
